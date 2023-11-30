e681d1adaf7ba002daa45b37b10bc2c9280580fe
chsua <csj1919@naver.com>
2023-11-30 14:21:44 +0900

    test: (#3) 컴포넌트에서 msw 작동 확인

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { worker } from '../mocks/worker';
+
 export default function Home() {
+  if (process.env.NODE_ENV === 'development') {
+    worker.start();
+  }
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('/example', { method: 'get' });
+      const data = await response.json();
+      setData(data);
+    })();
+  }, []);
+
   return (
     <main className="">
       This is main page. If you want to play any interaction, make some button
+      <p className="text-gray-800">{data}</p>
     </main>
   );
 }
--- mocks/example.ts
+++ mocks/example.ts
@@ -2,6 +2,6 @@ import { rest } from 'msw';
 
 export const example = [
   rest.get('/example', (req, res, ctx) => {
-    return res(ctx.status(200));
+    return res(ctx.status(200), ctx.json("hi! It's mockData"));
   }),
 ];
