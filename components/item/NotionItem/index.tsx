5b7d3ecde0e631a108eb4277e1f08ad97241de13
chsua <csj1919@naver.com>
2023-12-04 22:56:52 +0900

    feat: (#5) 노션 아이템에 클릭이벤트 추가
    - 더보기 메뉴 버튼 클릭 시 이벤트 버블링 발생으로 방어 코드 작성

--- components/item/NotionItem/index.tsx
+++ components/item/NotionItem/index.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes, MouseEventHandler } from 'react';
 
 import MoreMenuButton from '../../common/MoreMenuButton';
 import CircleLine from '../../common/CircleLine';
@@ -6,15 +6,34 @@ import RoundSquare from '../../common/RoundSquare';
 
 interface NotionItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   content: string;
+  handleNotionItemClick: () => void;
+  handleMoreNotionButtonClick: () => void;
 }
 
-export default function NotionItem({ content }: NotionItemProps) {
+export default function NotionItem({
+  content,
+  handleNotionItemClick,
+  handleMoreNotionButtonClick,
+}: NotionItemProps) {
+  const handleMoreNotionButtonClickWithoutBubbling: MouseEventHandler<
+    HTMLButtonElement
+  > = (event) => {
+    event.stopPropagation();
+    handleMoreNotionButtonClick();
+  };
+
   return (
     <RoundSquare size="sm">
-      <button className="w-[90%] flex flex-row items-center justify-between gap-2">
+      <button
+        className="w-[90%] flex flex-row items-center justify-between gap-2"
+        onClick={handleNotionItemClick}
+      >
         <CircleLine amount={1} />
         <p className="w-full truncate text-left">{content}</p>
-        <MoreMenuButton size="sm" />
+        <MoreMenuButton
+          size="sm"
+          onClick={handleMoreNotionButtonClickWithoutBubbling}
+        />
       </button>
     </RoundSquare>
   );
