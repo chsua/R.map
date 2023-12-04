4c381cb2f7d2ca443b317d6f432746853400681b
chsua <csj1919@naver.com>
2023-12-05 00:25:58 +0900

    feat: (#5) 노션들 목록이 있는 홈페이지 생성 (바텀시트 연결 미완)

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,34 @@
 import { useEffect, useState } from 'react';
 import { worker } from '../mocks/worker';
 
+import Title from '@components/common/Title';
+import CircleLine from '@components/common/CircleLine';
+import NotionList from '@components/item/NotionList';
+
 export default function Home() {
-  if (process.env.NODE_ENV === 'development') {
-    worker.start();
-  }
+  // if (process.env.NODE_ENV === 'development') {
+  //   worker.start();
+  // }
 
-  const [data, setData] = useState();
+  // const [data, setData] = useState();
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('/example', { method: 'get' });
-      const data = await response.json();
-      setData(data);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await fetch('/example', { method: 'get' });
+  //     const data = await response.json();
+  //     setData(data);
+  //   })();
+  // }, []);
 
   return (
-    <main className="">
-      This is main page. If you want to play any interaction, make some button
-      <p className="text-gray-800">{data}</p>
+    <main className="flex flex-col gap-5">
+      <Title content="개념" />
+      <CircleLine amount={8} />
+      <NotionList
+        handlePlusButtonClick={() => {}}
+        handleMoreMenuButtonClick={() => {}}
+        handleNotionItemClick={() => {}}
+      />
     </main>
   );
 }
