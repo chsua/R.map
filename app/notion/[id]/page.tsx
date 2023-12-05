9e00d76ab6af0d109ffcb7c80775f4ef3a8a66fb
chsua <csj1919@naver.com>
2023-12-05 19:34:14 +0900

    feat: (#5) 노션 상세 페이지 생성

--- app/notion/[id]/page.tsx
+++ app/notion/[id]/page.tsx
@@ -1,5 +1,59 @@
-import React from 'react';
+'use client';
+
+import React, { ReactNode, useState } from 'react';
+
+import BottomSheet from '@components/common/BottomSheet';
+import CircleLine from '@components/common/CircleLine';
+import Description from '@components/common/Description';
+import Title from '@components/common/Title';
+import NotionList from '@components/item/NotionList';
+import NotionForm from '@components/item/NotionForm';
+
+import { mockNotionBanana } from '@mocks/mockData/notionList';
+
+import { useRouter } from 'next/navigation';
+import { useRecentlyNotionContext } from '@components/context/RecentlyNotionContext';
+
+type Content = 'notionItemForm' | 'notionItemPlusMenu';
+
+const content: Record<Content, ReactNode> = {
+  notionItemForm: <NotionForm />,
+  notionItemPlusMenu: <></>,
+};
 
 export default function play({ params }: { params: { id: number } }) {
-  return <div>this is side page. </div>;
+  const router = useRouter();
+  const { addNotionItem } = useRecentlyNotionContext();
+  const [bottomSheetContent, setBottomSheetContent] = useState<Content | null>(
+    null,
+  );
+
+  //추후 fetch한 데이터로 수정
+  const { id, name, description, relatedNotionList } = mockNotionBanana;
+
+  const moveNotionItemPage = (id: number) => {
+    addNotionItem({ id, name });
+    router.push(`/notion/${id}`, { scroll: true });
+  };
+
+  return (
+    <main className="flex flex-col gap-5">
+      <Title content={name} />
+      <CircleLine amount={8} />
+      <Description content={description} />
+      <NotionList
+        notionList={relatedNotionList}
+        handlePlusButtonClick={() => setBottomSheetContent('notionItemForm')}
+        handleMoreMenuButtonClick={() =>
+          setBottomSheetContent('notionItemPlusMenu')
+        }
+        handleNotionItemClick={moveNotionItemPage}
+      />
+      {bottomSheetContent && (
+        <BottomSheet closeEvent={() => setBottomSheetContent(null)}>
+          {content[bottomSheetContent]}
+        </BottomSheet>
+      )}
+    </main>
+  );
 }
