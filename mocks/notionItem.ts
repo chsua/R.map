75453962445ade1343fd7b51388f62092eb85981
chsua <csj1919@naver.com>
2023-12-06 15:32:04 +0900

    feat: (#7) msw에서 노션 아이템 id에 맞는 mockData가지고 오도록 고도화

--- mocks/notionItem.ts
+++ mocks/notionItem.ts
@@ -1,9 +1,16 @@
 import { rest } from 'msw';
-import { mockNotionApple } from './mockData/notionList';
+import { findMockNotionItem } from './mockData/notionList';
 
 export const getNotionItem = [
   rest.get('/notions/:id', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(mockNotionApple));
+    const targetId = req.params.id;
+
+    if (typeof targetId === 'string') {
+      const mockData = findMockNotionItem(+targetId);
+      return res(ctx.status(200), ctx.json(mockData));
+    }
+
+    return res(ctx.status(404), ctx.json({}));
   }),
 ];
 
