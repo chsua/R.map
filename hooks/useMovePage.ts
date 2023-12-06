c1a803d06f3835f0e2925bfb841683535006cdd3
chsua <csj1919@naver.com>
2023-12-06 15:46:44 +0900

    feat: (#7) 상세페이지에서 홈페이지로 이동하는 nav 구현

--- hooks/useMovePage.ts
+++ hooks/useMovePage.ts
@@ -3,10 +3,15 @@ import { useRouter } from 'next/navigation';
 export const useMovePage = () => {
   const router = useRouter();
 
+  const moveMainPage = (callback?: () => void) => {
+    callback && callback();
+    router.push(`/`, { scroll: true });
+  };
+
   const moveNotionItemPage = (id: number, callback?: () => void) => {
     callback && callback();
     router.push(`/notion/${id}`, { scroll: true });
   };
 
-  return { moveNotionItemPage };
+  return { moveMainPage, moveNotionItemPage };
 };
