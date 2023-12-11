1dc70ec6f8e6371ed55787fe2bed736a7c8696ef
chsua <csj1919@naver.com>
2023-12-11 11:18:49 +0900

    feat: (#17) 실제 서버 URL env로 관리 및 연결하기

--- constants/url.ts
+++ constants/url.ts
@@ -1,9 +1,12 @@
+// const base = "";
+const base = process.env.NEXT_PUBLIC_BASE_URL;
+
 export const GET_URL = {
   NOTION_ITEM_MOCK: () => `/notions/:id`,
-  NOTION_ITEM: (id: number) => `/notions/${id}`,
+  NOTION_ITEM: (id: number) => `${base}/notions/${id}`,
 };
 
 export const POST_URL = {
   NOTION_ITEM_MOCK: () => `/notions`,
-  NOTION_ITEM: () => `/notions`,
+  NOTION_ITEM: () => `${base}/notions`,
 };
