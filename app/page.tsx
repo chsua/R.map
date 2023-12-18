d193ccdef9429bd3c7bdbb861ad2f3d859be2635
chsua <csj1919@naver.com>
2023-12-18 21:26:52 +0900

    feat: (#21) 메인페이지 노션그래프 목록 불러오기 api 연결

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import NotionList from '@components/item/NotionList';
 import Title from '@components/common/Title';
 import CircleLine from '@components/common/CircleLine';
 
-import { mockNotionBear, mockNotionSnowWhite } from '@mocks/mockData/notion';
 import { useNotionItemBottomSheet } from 'hooks/useNotionItemBottomSheet';
 import { useMovePage } from 'hooks/useMovePage';
 
+import { EssenceNotion } from 'types/notion';
+import { getFetch } from 'utils/fetch';
+import { GET_URL } from 'constants/url';
+
 export default function Home() {
+  const [data, setData] = useState<EssenceNotion[]>();
+
   const {
     handlePlusButtonClick,
     handleMoreMenuButtonClick,
@@ -17,21 +24,27 @@ export default function Home() {
 
   const { moveNotionItemPage } = useMovePage();
 
+  useEffect(() => {
+    (async () => {
+      const data = await getFetch<EssenceNotion[]>(GET_URL.NOTION_GRAPH_LIST());
+      setData(data);
+    })();
+  }, []);
+
   return (
-    <main className="flex flex-col gap-5">
-      <Title content="R:map" />
-      <CircleLine amount={8} />
-      <NotionList
-        style="md:grid-cols-2 lg:grid-cols-3"
-        notionList={[
-          ...mockNotionBear.relatedNotions,
-          ...mockNotionSnowWhite.relatedNotions,
-        ]}
-        handlePlusButtonClick={handlePlusButtonClick}
-        handleMoreMenuButtonClick={handleMoreMenuButtonClick}
-        handleNotionItemClick={moveNotionItemPage}
-      />
-      {bottomSheetComponent}
-    </main>
+    data && (
+      <main className="flex flex-col gap-5">
+        <Title content="R:map" />
+        <CircleLine amount={8} />
+        <NotionList
+          style="md:grid-cols-2 lg:grid-cols-3"
+          notionList={data}
+          handlePlusButtonClick={handlePlusButtonClick}
+          handleMoreMenuButtonClick={handleMoreMenuButtonClick}
+          handleNotionItemClick={moveNotionItemPage}
+        />
+        {bottomSheetComponent}
+      </main>
+    )
   );
 }
