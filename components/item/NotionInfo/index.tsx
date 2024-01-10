d78546e17abc504657758e2bdf116ca80ccd39e0
chsua <csj1919@naver.com>
2024-01-10 15:23:46 +0900

    feat: (#25) 노션 정보 컴포넌트 내 버튼들 children으로 전달하도록 수정

--- components/item/NotionInfo/index.tsx
+++ components/item/NotionInfo/index.tsx
@@ -1,16 +1,17 @@
-import RoundSquare from '@components/common/RoundSquare';
 import { NotionFolder } from 'types/notion';
 import NotionItem from '../NotionItem';
-import CircleLine from '@components/common/CircleLine';
+import { ReactNode } from 'react';
 
 interface NotionInfoProps {
   notion: NotionFolder;
   handleNotionItemClick?: () => void;
+  children?: ReactNode;
 }
 
 export default function NotionInfo({
   notion,
   handleNotionItemClick,
+  children,
 }: NotionInfoProps) {
   return (
     <div className="w-[90%] h-[90%] flex gap-5 flex-col m-auto">
@@ -18,14 +19,7 @@ export default function NotionInfo({
         content={notion.name}
         handleNotionItemClick={handleNotionItemClick}
       />
-      <button className="flex gap-3 items-center text-sm">
-        <CircleLine amount={1} />
-        <span>추후 폴더 합치기 버튼</span>
-      </button>
-      <button className="flex gap-3 items-center text-sm">
-        <CircleLine amount={1} />
-        <span>추후 폴더 삭제 버튼</span>
-      </button>
+      {children}
     </div>
   );
 }
