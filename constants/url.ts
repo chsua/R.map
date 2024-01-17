68fc087a80b03b4675ede704c2ba20aeabecddb4
chsua <csj1919@naver.com>
2024-01-17 11:39:00 +0900

    feat: (#27) 노션 폴더 api url 수정

--- constants/url.ts
+++ constants/url.ts
@@ -6,8 +6,8 @@ export const GET_URL = {
   NOTION_ITEM: (id: number) => `${base}/notions/${id}`,
   NOTION_FOLDER_LIST_MOCK: () => `/notion-folders`,
   NOTION_FOLDER_LIST: () => `${base}/notion-folders`,
-  NOTION_LIST_IN_FOLDER_MOCK: () => `/notion-folders/:id/notions`,
-  NOTION_LIST_IN_FOLDER: (id: number) => `${base}/notion-folders/${id}/notions`,
+  NOTION_FOLDER_MOCK: () => `/notion-folders/:id`,
+  NOTION_FOLDER: (id: number) => `${base}/notion-folders/${id}`,
 };
 
 export const POST_URL = {
