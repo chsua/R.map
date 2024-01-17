ab2118dec168c185f0125ec377864c5df8cf1b7d
chsua <csj1919@naver.com>
2024-01-17 18:25:46 +0900

    feat: (#27) fetch시 res.ok아니면 res를 오류로 던지는 코드 각주 생성

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -1,10 +1,20 @@
+export interface FetchError {
+  status: string;
+  message: any;
+}
+
 export const getFetch = async <T>(url: string): Promise<T> => {
   const response = await fetch(url, {
     method: 'get',
     cache: 'no-store',
   });
 
-  if (!response.ok) throw new Error('error');
+  // 자꾸 런타임 오류가 나서 일단 각주처리
+  // if (!response.ok) {
+  //   throw new Error(
+  //     JSON.stringify({ status: response.status, message: response.text() }),
+  //   );
+  // }
 
   return response.json();
 };
@@ -21,7 +31,10 @@ export const fetchWithoutGet = async <T, R>(
     cache: 'no-store',
   });
 
-  if (!response.ok) throw new Error('error');
+  // if (!response.ok)
+  //   throw new Error(
+  //     JSON.stringify({ status: response.status, message: response.text() }),
+  //   );
 
   return response.status === 201 ? response.json() : response;
 };
