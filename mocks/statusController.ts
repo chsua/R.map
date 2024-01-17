1b26cb623b505852644577fcd2ebc7e605c2f297
chsua <csj1919@naver.com>
2024-01-17 18:24:36 +0900

    feat: (#27) 모킹 테스트 상태코드 분리하여 관리

---
+++ mocks/statusController.ts
@@ -0,0 +1,15 @@
+export const mockingStatus = {
+  get: {
+    notionFolderList: 400,
+    notionFolder: 200,
+    notion: 200,
+  },
+  post: {
+    notionFolder: 200,
+    notion: 200,
+  },
+  delete: {
+    notionFolder: 200,
+    notion: 200,
+  },
+};
